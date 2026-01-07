77b1557ed0cfbb41d8c2062a67ab96c045a34c14
Lokesh Morla <lokeshmorla49@gmail.com>
2026-01-07 17:05:56 +0530

    datatypes and variables

---
+++ python-day2.py
@@ -0,0 +1,16 @@
+datatypes
+day1="welcome to codegnan class"
+name="Lokesh"
+weight_decimal=54.25
+age=21
+list1=[1,2,28,37,"hi",10.21]#list ofv items
+tuple1=(10,21,"code")#tuple of items
+set={1,10,21,21,10}#set of items
+my_marks={'Python':10,'html':21,'css':15}
+print(day1)
+print("My name is: ",name)
+print("My age is: ",age)
+print("My marks: ",my_marks)
+print(list1)
+print(tuple1)
+print(set)
