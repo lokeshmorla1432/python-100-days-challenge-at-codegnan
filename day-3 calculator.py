0ebc1c6ff6ff0d54aa2b1fc3162adc47b0c1f8e6
Lokesh Morla <lokeshmorla49@gmail.com>
2026-01-21 13:35:47 +0530

    Calculator

---
+++ day-3 calculator.py
@@ -0,0 +1,15 @@
+#simple calculator operations
+#variable and operations
+a=10
+b=20
+addition=a+b
+print("sum of two numbers is:", addition)
+print("subtraction of two numbers is:", a-b)
+print("Multiplation of two numbers is:", a*b)
+print("Division of two numbers is:", b/a)
+print("Floor division of two numbers:", b//a)
+print("Modulo division of two numbers:",b%a)
+print("power of two numbers:", b**a)
+
+
+print("10 lessless 20 is:",10<20)
